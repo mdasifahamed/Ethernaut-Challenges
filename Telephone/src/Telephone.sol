ff9302d793c6159862b09b2c1feef440475988ed
mdasifahamed <mdasifahamed89@yahoo.com>
2024-02-19 00:54:23 +0600

    Contarct Added

---
+++ Telephone/src/Telephone.sol
@@ -0,0 +1,14 @@
+
+contract Telephone {
+
+  address public owner;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  function changeOwner(address _owner) public {
+    if (tx.origin != msg.sender) {
+      owner = _owner;
+    }
+  }
