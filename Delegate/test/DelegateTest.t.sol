b8f9668b653fcc97c86ae23d178971b7783a1f5a
mdasifahamed <mdasifahamed89@yahoo.com>
2024-02-23 02:11:49 +0600

    Attack Done With Attacker Contract

--- Delegate/test/DelegateTest.t.sol
+++ Delegate/test/DelegateTest.t.sol
@@ -3,15 +3,19 @@ pragma solidity ^0.8.0;
 
 import{Test,console} from "forge-std/Test.sol";
 import {Delegate, Delegation} from "../src/Delegates.sol";
+import{Attacker} from "../src/Attacker.sol";
+
 
 contract DelegateTest is Test {
 
     address DelegateContractOwner = makeAddr("DOwner");
     address AttackerOwner = makeAddr("attacker");
+    
 
 
     Delegate delegate;
     Delegation delegation;
+    Attacker attacker;
     function setUp() public{
 
 
@@ -20,6 +24,10 @@ contract DelegateTest is Test {
         delegate = new Delegate(DelegateContractOwner);
 
         delegation = new Delegation(address(delegate));
+        vm.stopPrank();
+
+        vm.startPrank(AttackerOwner);
+        attacker = new Attacker(address(delegation));
         vm.stopPrank();
 
      
@@ -32,6 +40,25 @@ contract DelegateTest is Test {
         assertEq(delegation.owner(), DelegateContractOwner);
 
     }
+
+
+    function test_AttackWithContract() public {
+        //Before Attack 
+
+        assertEq(delegation.owner(), DelegateContractOwner);
+
+        vm.startPrank(AttackerOwner);
+        attacker.attack();
+        vm.stopPrank();
+        
+        // After Attack The Attackeer Contract Should be The Owner
+        assertEq(delegation.owner(), address(attacker));
+
+
+
+        
+    }
+
 
 
 
